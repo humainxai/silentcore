b9a60ef53939a0f382b7269a7e1799e728b4132b
humainxai <naamatech@gmail.com>
2025-06-03 00:49:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Welcome to SilentCore</h1>
+      <p>This is the beginning of human + AI synergy.</p>
+    </div>
+  );
+}
